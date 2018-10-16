5bd6647e363ece8562403c9df219b74f8fdb8c59
SG <sgng1@e.ntu.edu.sg>
2018-10-16 08:12:07 +0100

    cleared exercise

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const app = express();
 app.use(express.static('client'));
 app.use(express.json())
 
-let songs = [];
+let songs = [{id:1,name:"fire"},{id:2,name:"ice"}];
+
+app.param("songid", function(req,res,next,songid){
+  let song = songs.find(song => song.id == parseInt(songid));
+  req.song = song
+  next()
+})
 
 //return list of all songs
 app.get('/songs', (req, res) => {
@@ -23,9 +29,9 @@ app.post('/songs', (req, res) => {
 });
 
 //return a song with id 
-app.get('/songs/:id', (req, res) => {
-  let song = songs.find(song => song.id == parseInt(req.params.id));
-  res.status(200).json(song);
+app.get('/songs/:songid', (req, res) => {
+  const song = req.song
+  res.send("Got the request for user " + song.name);
 });
 
 //edit a song with id, and return edited song
